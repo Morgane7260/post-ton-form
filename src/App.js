f095dfaff7202dcbb33886a79e24aeb9a59e18ee
Morgane7260 <morgane7260@gmail.com>
2019-12-16 11:48:55 +0100

    soluce oklm

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormEmployee from './components/FormEmployee';
+
+function App() {
+  return (
+    <div>
+      <FormEmployee />
+    </div>
+  );
+}
+
+export default App;
